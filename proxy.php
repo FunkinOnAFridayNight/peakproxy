9ac348b2f3777a339d702cd190e27c8f49fc6aaf
FunkinOnAFridayNight <87614746+FunkinOnAFridayNight@users.noreply.github.com>
2025-10-25 04:03:27 -0700

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,11 @@
+<?php
+$url = $_GET['url'];
+
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+    $content = file_get_contents($url);
+    header('Content-Type: text/html');
+    echo $content;
+} else {
+    echo 'Invalid URL';
+}
+?>
